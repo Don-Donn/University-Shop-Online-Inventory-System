cc51bc50789810a7764ea4d0928ba17a8df21f97
Don-Donn <21-37287@g.batstate-u.edu.ph>
2023-10-25 20:03:47 +0800

    edited login and admin login php, drafting userSide.php

--- pages/loginAdmin.php
+++ pages/loginAdmin.php
@@ -2,11 +2,11 @@
 <html>
 <head>
 
-    <title>Login Page</title>
+    <title>Admin Login Page</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="../css/login.css">
+    <link rel="stylesheet" href="../css/loginAdmin.css">
 
 </head>
 <body>
@@ -27,16 +27,16 @@
 
     <div class="login_container">
     <form action="login.php" method="post">
-        <p class="please-login">Please Login</p>
+        <p class="admin-login">Admin Login</p>
 
-        <input type="text" id="email" name="email" placeholder="  Email address" required><br>
+        <input type="text" id="email" name="email" placeholder="  Admin Username" required><br>
 
-        <input type="password" id="password" name="password" placeholder="  Password" required><br>
+        <input type="password" id="password" name="password" placeholder="  Admin Password" required><br>
 
         <button type="submit" name="login_user" id="login-button">Login</button>
 
-        <p class="login-admin">
-            <a class="login-admin" href="#">Login as an ADMIN?</a>
+        <p class="admin-login">
+            <a class="login-customer" href="#">Login as a CUSTOMER?</a>
         </p>
     </form>
     </div>
--- pages/userSide.php
+++ pages/userSide.php
@@ -1,48 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>UNIVERSITY SHOP</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-<style>
-h1{
-    color:red;
-}   
-table,th{
-  font-size: 30px;
-  text-align: center;
-  border: 4px solid;
-  border-color:red
-}
-td{
-    font-size: 20px;
-}
 
-</style>
+    <link rel="stylesheet" href="../css/userSide.css">
+
 </head>
 
 
 <body>
-    <nav class="navbar bg-body-tertiary">
-        <div class="container" >
-        <img src="./images/BSU Logo.png" alt="Logo" width="100" height="90">
-        <h1 style="font-family: 'Times New Roman', serif;">BATANGAS STATE UNIVERSITY SHOP (LIPA CAMPUS)</h1>
-            <a class="btn" href="login.php">
-                <img type="button" src="./images/Logout (2).png" alt="Logout" width="40" height="30">
-            </a>
+    <nav>
+        <img id="bsu-logo" src="../icons/bsuLogo.png" alt="Logo" width="100" height="90" >
+
+        <div>
+            <p id = "shop-txt">UNIVERSITY SHOP</p>
         </div>
+        <div id = "location">
+            <h4>BatStateU Lipa Campus</h4>
+        </div>
+        <a class="logout-btn" href="login.php">
+            <img type="button" id="logout-icon" src="../icons/logout2.png" alt="Logout" width="40" height="30" ">
+        </a>
     </nav>
 
     <br>
     </br>
 
-    <table class="table" >
+    <table class="product-table" >
     <thead>
         <tr>
             <th scope="col">Product</th>
             <th scope="col">Size</th>
-            <th scope="col">Available</th>
+            <th scope="col">Stock(s)</th>
             <th scope="col">Price</th>
         </tr>
         <tr>
