128c69835e4cecb0422ddbaa0c2968819cb66b54
Don-Donn <21-37287@g.batstate-u.edu.ph>
2023-10-25 14:14:20 +0800

    edited login.php and login.css

--- pages/login.php
+++ pages/login.php
@@ -1,45 +1,44 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <title>Login Page</title>
-    <link rel="stylesheet" href="login.css"/>
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="../css/login.css">
 
 </head>
 <body>
 <nav class="navbar bg-body-tertiary">
     <div class="container">
-        <div class="rgo">
+        <div class="header" id = "rgo">
             <h1>RESOURCE GENERATION OFFICE</h1>
         </div>
-        <div class="location">
-            <h3>Room 202, CECS Bldg., BatStateU Lipa, Marawoy, Lipa City</h3>
+        <div class="header" id = "location">
+            <h3>BatStateU Lipa, Marawoy, Lipa City</h3>
         </div>
     </div>
 </nav>
 <br>
 </br>
 
-    <div class="univ">
-    <h2>University Shop Login</h2>
+    <div id ="univ_text">
+        <h2>University Shop Login</h2>
     </div>
 
-    <div class="login">
+    <div class="login_container">
     <form action="login.php" method="post">
-        <h2>Please Login</h2>
+        <p class="please-login">Please Login</p>
 
-        <label for="email">EMAIL</label>
-        <input type="text" id="email" name="email" placeholder="Email address" required><br>
+        <input type="text" id="email" name="email" placeholder="  Email address" required><br>
 
-        <label for="password">PASSWORD</label>
-        <input type="password" id="password" name="password" placeholder="Password" required><br>
+        <input type="password" id="password" name="password" placeholder="  Password" required><br>
 
         <button type="submit" name="login_user">Login</button>
 
-        <p >
-            <a href="#">Login as an ADMIN?</a>
+        <p class="login-admin">
+            <a class="login-admin" href="#">Login as an ADMIN?</a>
         </p>
     </form>
     </div>
